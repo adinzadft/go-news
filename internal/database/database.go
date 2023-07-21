9c87c02805ca2f7151fea00addf6f22398e17417
adinzadft <firdaustirta13@gmail.com>
2023-07-21 15:07:30 +0700

    add os.Getenv

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,13 +1,15 @@
 package mysql
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func DB() *gorm.DB {
 
-	dsn := "zidan1102:2011&@d!$04@tcp(localhost:3306)/go-news?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
